26c094c0c4ea8f9549367ab2b335ff70eb53f3c1
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-01-05 12:39:52 +0100

    za16

---
+++ src/main/java/pl/sdacademy/wiosnademo/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.wiosnademo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(path = "/index")
+    public String getIndex(final ModelMap modelMap) {
+        return "index";
+    }
+}
