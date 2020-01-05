b8db14edd38a6bed6d0d059627c5f54f04e7deb3
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-01-05 10:39:02 +0100

    zad15

--- src/main/java/pl/sdacademy/wiosnademo/controllers/GenericExceptionHandler.java
+++ src/main/java/pl/sdacademy/wiosnademo/controllers/GenericExceptionHandler.java
@@ -1,6 +1,8 @@
 package pl.sdacademy.wiosnademo.controllers;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,9 +10,20 @@ import pl.sdacademy.wiosnademo.model.ErrorMessage;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GenericExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleMethodArgumentNotValidException(final MethodArgumentNotValidException exp) {
+        return ErrorMessage.builder()
+                .message("Failed to validate object " + exp.getBindingResult().getTarget().getClass().getSimpleName())
+                .details(exp.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
