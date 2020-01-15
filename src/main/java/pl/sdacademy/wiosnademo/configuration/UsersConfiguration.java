a7075deec2aa1a69461cb914e49a06bde8e91902
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-01-15 15:47:09 +0100

    zad29

--- src/main/java/pl/sdacademy/wiosnademo/configuration/UsersConfiguration.java
+++ src/main/java/pl/sdacademy/wiosnademo/configuration/UsersConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,6 +17,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class UsersConfiguration {
 
-    private String userA;
-    private String passwordA;
+    private List<String> users;
+    private List<String> passwords;
 }
