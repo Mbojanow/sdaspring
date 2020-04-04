c669feee2dae14a920ae0b91cb949b080e71f50c
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-04-04 13:49:15 +0200

    da1mid4

--- src/main/java/pl/sdacademy/wiosnademo/controllers/MessageController.java
+++ src/main/java/pl/sdacademy/wiosnademo/controllers/MessageController.java
@@ -1,22 +1,26 @@
 package pl.sdacademy.wiosnademo.controllers;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import pl.sdacademy.wiosnademo.domain.Message;
+import pl.sdacademy.wiosnademo.domain.Messages;
 import pl.sdacademy.wiosnademo.repostories.MessageRepository;
 
-@Controller
-@ResponseBody
-@RequestMapping("/api/messages")
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping(value = "/api/messages",
+    consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, // Content-Type
+    produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }) // Accept
 public class MessageController {
 
   private final MessageRepository messageRepository;
@@ -25,24 +29,28 @@ public class MessageController {
     this.messageRepository = messageRepository;
   }
 
-  @RequestMapping(method = RequestMethod.GET) // /api/messages
-  public List<Message> getAllMessages() {
-    return messageRepository.getAll();
+  //@RequestMapping(method = RequestMethod.GET) // /api/messages
+  @GetMapping
+  public Messages getAllMessages() {
+    return new Messages(messageRepository.getAll());
   }
 
-  @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+  //@RequestMapping(path = "/{id}", method = RequestMethod.GET)
+  @GetMapping(path = "/{id}")
   public Message getMessageById(@PathVariable(name = "id") final Long id) {
     return messageRepository.findById(id).orElse(null);
   }
 
-  @RequestMapping(method = RequestMethod.POST)
+  //@RequestMapping(method = RequestMethod.POST)
+  @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Message createMessage(@RequestBody final Message message) {
     return messageRepository.createMessage(message);
   }
 
   //api/messages/14
-  @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+  //@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+  @DeleteMapping(path = "/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteById(@PathVariable(name = "id") final Long id) {
     messageRepository.deleteById(id);
--- src/main/java/pl/sdacademy/wiosnademo/domain/Message.java
+++ src/main/java/pl/sdacademy/wiosnademo/domain/Message.java
@@ -3,6 +3,8 @@ package pl.sdacademy.wiosnademo.domain;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,16 @@ import lombok.NoArgsConstructor;
 public class Message {
 
   private Long id;
+
+  @JsonProperty("consignor")
   private String from;
+
+  @JsonProperty("recipent")
   private String to;
   private String value;
   private LocalDateTime sendDate;
 
+  @JsonIgnore
   public boolean isInFuture() {
     return sendDate.isAfter(LocalDateTime.now());
   }
