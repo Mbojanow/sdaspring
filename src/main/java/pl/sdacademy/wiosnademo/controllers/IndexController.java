d7c946edc8d7071eda5b60cde3013a4a0cbe9671
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-04-05 11:04:04 +0200

    day2mid

--- src/main/java/pl/sdacademy/wiosnademo/controllers/IndexController.java
+++ src/main/java/pl/sdacademy/wiosnademo/controllers/IndexController.java
@@ -4,7 +4,11 @@ package pl.sdacademy.wiosnademo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.sdacademy.wiosnademo.domain.NameForm;
 
 @Controller
 @RequestMapping(path = "/index")
@@ -13,6 +17,14 @@ public class IndexController {
   @GetMapping
   public String showHomePage(final ModelMap modelMap) {
     modelMap.addAttribute("btnText", "Click ME!");
+    modelMap.addAttribute("nameForm", new NameForm());
     return "index";
+  }
+
+  @PostMapping
+  public String handleNameChange(@ModelAttribute(name = "nameForm") final NameForm nameForm,
+                                 final ModelMap modelMap) {
+    modelMap.addAttribute("fullName", nameForm.getFirstName() + " " + nameForm.getLastName());
+    return showHomePage(modelMap);
   }
 }
