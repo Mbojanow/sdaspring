6b13b98fb66709b67e6e19ad56c2fbf41356eef2
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-04-05 10:23:29 +0200

    day2mid

--- src/main/java/pl/sdacademy/wiosnademo/controllers/IndexController.java
+++ src/main/java/pl/sdacademy/wiosnademo/controllers/IndexController.java
@@ -2,6 +2,7 @@ package pl.sdacademy.wiosnademo.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
   @GetMapping
-  public String showHomePage() {
+  public String showHomePage(final ModelMap modelMap) {
+    modelMap.addAttribute("btnText", "Click ME!");
     return "index";
   }
 }
