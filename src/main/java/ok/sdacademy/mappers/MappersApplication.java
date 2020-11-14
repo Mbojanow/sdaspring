dd7915359603126b47faf6d8292b2d377c7d26dc
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-11-14 08:45:45 +0100

    mappers start

---
+++ src/main/java/ok/sdacademy/mappers/MappersApplication.java
@@ -0,0 +1,13 @@
+package ok.sdacademy.mappers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappersApplication.class, args);
+	}
+
+}
